7acaeeb4279baa5a4750c1937ec2adf62324a550
standardvaultremitb <mike101.uk@gmail.com>
2025-12-30 23:25:35 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,16 +1,19 @@
 import { signInWithEmailAndPassword } from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import { auth } from "./firebase.js";
+
 window.login = function () {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
   const error = document.getElementById("error");
 
-  signInWithEmailAndPassword(window.auth, email, password)
+  signInWithEmailAndPassword(auth, email, password)
     .then(() => {
       window.location.href = "dashboard.html";
     })
-    .catch(() => {
-      error.textContent = "Invalid login credentials";
+    .catch((err) => {
+      error.textContent = err.message;
     });
 };
+
